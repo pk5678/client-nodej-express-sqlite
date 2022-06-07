fc932a85ecc9804166a7cbf6f2b5bbd9db354aaa
pk5678 <email.pramod@gmail.com>
2022-06-07 19:31:20 +0530

    source folder
    source code folder

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import AddUser from "./AddUser";
+import ListUser from "./ListUser";
+import "./App.css"
+
+function App() {
+    return (
+        <div className="App-header">
+            <AddUser/>
+            <br/>
+            <ListUser/>
+        </div>
+    );
+}
+
+export default App;
